e24cf70aadd7d1284d4a5e7c8b1fa1339509fdb5
edu <cesconettoedu@gmail.com>
2023-06-29 23:45:03 -0400

    start project

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, TouchableOpacity, Text, TextInput, StyleSheet, Image, KeyboardAvoidingView, Pressable, Modal, ImageBackground } from "react-native";
 
 export default function App() {
+
+  const [metroS, setMetroS] = useState('metro');
+  const [metro, setMetro] = useState(0);
+  const [ft, setFt] = useState(0);
+  const [inches, setInches] = useState(0);
+
+
+  const metroPlu = (metro) => {
+    if(metro > 1){
+      setMetroS('metros')
+    } else {
+      setMetroS('metro')
+    }
+    
+  }
+
+  const metroToFt = (metro) => {
+    let ft = 0
+    ft = (metro * 3.28084).toFixed(2);
+    setFt(ft)
+  }
+
+  const metroToInches = (metro) => {
+    let inches = 0
+    inches = (metro * 39.3701).toFixed(2);
+    setInches(inches)
+  }
+
+
+
+  useEffect(() => {
+    metroPlu(metro);
+    metroToFt(metro);
+    metroToInches(metro);
+
+  },[metro])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <KeyboardAvoidingView
+        style={styles.keyboardAvoid}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <Text>{metro} {metroS} = {ft} ft = {inches} inches</Text>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder=" metro"
+          placeholderTextColor="#4a4e69"
+          keyboardType="numeric"
+          maxLength={5}
+          value={metro}
+          onChangeText={setMetro}
+        />
+
+      </KeyboardAvoidingView>
+    
     </View>
   );
 }
@@ -16,5 +73,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderRadius: 5,
+    margin: 5,
+    height: 40,
+    borderColor: "#4a4e69",
+    borderWidth: 1,
+    paddingLeft: 10
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# React_Native_Measure
+Just a project to keep the measures that i need in same page in a APP

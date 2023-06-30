1d856e7718598493a5124a6e4a73d77743466920
edu <cesconettoedu@gmail.com>
2023-06-30 18:05:03 -0400

    start put icons

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, StyleSheet, KeyboardAvoidingView } from "react-native";
+import { View, Text, TextInput, StyleSheet, KeyboardAvoidingView, TouchableOpacity,Image } from "react-native";
+import On from './assets/tests/on.png'
+import Off from './assets/tests/off.png'
+
 
 export default function App() {
 
   const [metroS, setMetroS] = useState('metro');
   const [metro, setMetro] = useState(0);
+  const [metroOff, setMetroOff] = useState(On);
 
   const metroPlu = (metro) => {
     if(metro > 1){
@@ -13,6 +17,14 @@ export default function App() {
     } else {
       setMetroS('metro')
     }  
+  }
+
+  const metroOnOff = () => {
+    if(metroOff === On){
+      setMetroOff(Off)
+    } else {
+      setMetroOff(On)
+    }
   }
 
   useEffect(() => {
@@ -33,22 +45,67 @@ export default function App() {
           style={styles.keyboardAvoid}
           behavior={Platform.OS === "ios" ? "padding" : "height"}
         >
+          <View style={styles.metroContainer}>
+            <TouchableOpacity style={styles.dots} onPress={() => metroOnOff() }>
+              <Image
+                source={metroOff}
+                style={{ width: 90, height: 25 }}
+                />
+            </TouchableOpacity>
+            <Text> {(metro * 3.28084).toFixed(2)} ft</Text>
+          </View>
 
-          <Text> {(metro * 3.28084).toFixed(2)} ft</Text>
-          <Text> {(metro * 39.3701).toFixed(2)} inches</Text>
-          <Text>{metro} {metroS}</Text>
-          <Text>{metro / 100} cm</Text>
-          <Text>{metro / 1000} mm</Text>
-          <TextInput
-            style={styles.input}
-            underlineColorAndroid="transparent"
-            placeholder=" metro(s)"
-            placeholderTextColor="#D3D3D3"
-            keyboardType="numeric"
-            maxLength={8}
-            value={metro}
-            onChangeText={setMetro}
-            />
+           <View style={styles.metroContainer}>
+            <TouchableOpacity style={styles.dots} onPress={() => metroOnOff() }>
+              <Image
+                source={metroOff}
+                style={{ width: 90, height: 25 }}
+                />
+            </TouchableOpacity>
+            <Text> {(metro * 39.3701).toFixed(2)} inches</Text>
+          </View>
+
+          <View style={styles.metroContainer}>
+            <TouchableOpacity style={styles.dots} >
+              <Image
+                source={On}
+                style={{ width: 90, height: 25 }}
+                />
+            </TouchableOpacity>
+            <Text>{metro} {metroS}</Text>
+          </View>
+
+          <View style={styles.metroContainer}>
+            <TouchableOpacity style={styles.dots} onPress={() => metroOnOff() }>
+              <Image
+                source={metroOff}
+                style={{ width: 90, height: 25 }}
+                />
+            </TouchableOpacity>
+            <Text>{metro / 100} cm</Text>
+          </View>
+          
+          <View style={styles.metroContainer}>
+            <TouchableOpacity style={styles.dots} onPress={() => metroOnOff() }>
+              <Image
+                source={metroOff}
+                style={{ width: 90, height: 25 }}
+                />
+            </TouchableOpacity>
+            <Text>{metro / 1000} mm</Text>
+          </View>
+         
+
+            <TextInput
+              style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder=" metro(s)"
+              placeholderTextColor="#D3D3D3"
+              keyboardType="numeric"
+              maxLength={8}
+              value={metro}
+              onChangeText={setMetro}
+              /> 
 
         </KeyboardAvoidingView>
       </View>
@@ -73,7 +130,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     paddingLeft: 10
   },
-  measure: {
-    
+  metroContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
   }
 });

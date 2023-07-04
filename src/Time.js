7e77ecc27a2a3b55164df09fd6498c1ae33ba433
Eduardo <cesconettoedu@gmail.com>
2023-07-04 14:00:34 -0400

    set the time separeted

--- src/Time.js
+++ src/Time.js
@@ -1,80 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, StyleSheet, KeyboardAvoidingView, TouchableOpacity,Image } from "react-native";
-
-
-
+import { View, Text, TextInput, StyleSheet, Button, SafeAreaView } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function Time() {
+  const [datePst, setDatePst] = useState(new Date(1598051730000));
+  const [modePst, setModePst] = useState('date');
+  const [showPst, setShowPst] = useState(false);
 
- 
+  const [dateEst, setDateEst] = useState(new Date(1598051730000));
+  const [modeEst, setModeEst] = useState('date');
+  const [showEst, setShowEst] = useState(false);
+
+
+  //////////////     PST    ////////////////////////////
+  const onChangePst = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShowPst(false);
+    setDatePst(currentDate);
+  };
+
+  const showModePst = (currentMode) => {
+    setShowPst(true);
+    setModePst(currentMode);
+  };
+
+  const showTimepickerPst = () => {
+    showModePst('time');
+  };
+
+
+  //////////////     EST    ////////////////////////////
+  const onChangeEst = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShowEst(false);
+    setDateEst(currentDate);
+  };
+
+  const showModeEst = (currentMode) => {
+    setShowEst(true);
+    setModeEst(currentMode);
+  };
+
+  const showTimepickerEst = () => {
+    showModeEst('time');
+  };
+
+
+
+
+
+
+
 
   return (
-    <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Text>Time Converter</Text>
+    <SafeAreaView>
+    
+    <View style={styles.timeContainer}>
 
-        <View style={styles.timeContainer}>
+      <View>
+        <Text>Pacific{'\n'}PDT/PST</Text>
+        <Text>{datePst.getHours().toLocaleString()}:{datePst.getMinutes().toLocaleString()}</Text>
+        {showPst && (
+          <DateTimePicker
+          testID="dateTimePicker"
+          value={datePst}
+          mode={modePst}
+          is24Hour={true}
+          onChange={onChangePst}
+          />
+          )}
+        <Button onPress={showTimepickerPst} title="Show time picker!" />
+      </View>
 
-          <View style={styles.specificTime}>
-            <Text>Pacific{'\n'}PDT/PST</Text>
-            <TextInput
-                style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Pacific"
-                placeholderTextColor="#D3D3D3"
-                numeric
-                keyboardType="numeric"
-                maxLength={8}
-                value=""
-                //onChangeText={setNumVal}
-                onChangeText=""
-                // value={numVal.toString()}
-            /> 
-          </View>
 
-          <View style={styles.specificTime}>
-            <Text>Eastern{'\n'}EDT/EST</Text>
-            <TextInput
-                style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Eastern"
-                placeholderTextColor="#D3D3D3"
-                numeric
-                keyboardType="numeric"
-                maxLength={8}
-                value=""
-                onChangeText=""
-            /> 
-          </View>
-        </View>
+      <View>
+        <Text>Eastern{'\n'}EDT/EST</Text>
+        <Text>{dateEst.getHours().toLocaleString()}:{dateEst.getMinutes().toLocaleString()}</Text>
+        {showEst && (
+          <DateTimePicker
+          testID="dateTimePicker"
+          value={dateEst}
+          mode={modeEst}
+          is24Hour={true}
+          onChange={onChangeEst}
+          />
+          )}
+        <Button style={{}} onPress={showTimepickerEst} title="Show time picker!" />
 
-   
-    </View>
+
+
+      </View>
+
+    </View>   
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f6f6f6',
-    margin: 10,
-    justifyContent: 'center',
-  },
-  input: {
-    borderRadius: 5,
-    margin: 5,
-    height: 40,
-    width: 100,
-    borderColor: "#4a4e69",
-    borderWidth: 1,
-    paddingLeft: 10
-  },
   timeContainer: {
     flexDirection: 'row',
+    justifyContent: 'center',
+    justifyContent: 'space-around',
+
   },
-  specificTime: {
-    alignItems: 'center'
-  }
- 
 });
